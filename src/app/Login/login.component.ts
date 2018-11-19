dd0c83854ebec998334b150724f09e91dffb4fea
veysel <vinanc72@gmail.com>
2018-11-19 23:18:59 +0300

    Login and home components added

---
+++ src/app/Login/login.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.template.html'
+})
+export class LoginComponent {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Login/login.component';
+import { HomeComponent } from './Home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
