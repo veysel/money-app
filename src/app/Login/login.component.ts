6c7592486579497795dd3fad5506b54066a76ff4
veysel <vinanc72@gmail.com>
2018-12-19 00:42:48 +0300

    Login page changed

--- src/app/Login/login.component.ts
+++ src/app/Login/login.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './login.template.html'
 })
 export class LoginComponent {
+
+  public login(inputCode: any): boolean {
+    //console.log(inputCode.value);
+    return false;
+  }
+
 }
