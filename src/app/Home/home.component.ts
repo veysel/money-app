1a26de6a3456fe63e207d527a5df6a4173528615
veysel <vinanc72@gmail.com>
2019-02-19 23:57:22 +0300

    Logout module added

--- src/app/Home/home.component.ts
+++ src/app/Home/home.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { MoneyService } from '../Common/Service/money.service';
+import { StorageService } from '../Storage/Services/storage.service';
 
 @Component({
   selector: 'home',
   templateUrl: './home.template.html'
 })
 export class HomeComponent {
+
+  constructor(
+    private _moneyService: MoneyService,
+    private _storageService: StorageService,
+    private _router: Router,
+  ) { }
+
+  public LogoutButtonClick(): void {
+    this._storageService.ClearStorage();
+    this._router.navigate(["/login"]);
+  }
+
 }
